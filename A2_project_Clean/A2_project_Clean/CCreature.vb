c6c30ca66a6d8703d07d561a6bd1a2f281ae84ec
pjamieson99 <32102111+pjamieson99@users.noreply.github.com>
2017-11-29 13:56:53 +0000

    Add files via upload
    created creature function onto the program, added fitness and timer, can now put multiple animals in at once, however they seem to bug out when doing so, rnd function repeats every time it is called

---
+++ A2_project_Clean/A2_project_Clean/CCreature.vb
@@ -0,0 +1,168 @@
+﻿Public Class CCreature
+    Dim NumOfLegs As Integer
+    Dim NumOfLayers As Integer
+    Public Body As CBody
+    Dim line(NumOfLegs - 1, NumOfLayers - 1) As CLeg
+    Dim Joint(NumOfLegs - 1, NumOfLayers - 1) As CJoint
+    Dim Floor As CFloor
+    Public Fitness As Double
+    Public DeadCounter As Integer
+    Public TimeCounter As Integer
+
+    Sub New(B As CBody, L(,) As CLeg, J(,) As CJoint, F As CFloor)
+        NumOfLayers = 2
+        NumOfLegs = 3
+        Body = B
+        line = L
+        Joint = J
+        Floor = F
+        For y = 0 To NumOfLayers - 1
+            For x = 0 To NumOfLegs - 1
+                Body.BodyPoints(line(x, y).LP1, x)
+            Next
+        Next
+        Fitness = 0
+    End Sub
+
+    Sub NextMove(g As Graphics)
+        TimeCounter += 1
+        Floor.Draw(g)
+
+        'Sets previous points to variable used to calculate friction
+        For y = 0 To 1
+            For x = 0 To 2
+                line(x, y).OldPoint1 = line(x, y).LP1
+                line(x, y).OldPoint2 = line(x, y).LP2
+            Next
+        Next
+
+        'Find the side of the point touching the floor compared to the centre of mass
+        Body.Rightside = False
+        Body.Leftside = False
+        For y = 0 To NumOfLayers - 1
+            For x = 0 To NumOfLegs - 1
+
+                line(x, y).FindSideLegisOn(Body, Floor)
+
+            Next
+        Next
+
+        'Find the pivot point for the body
+        Body.Pivot.Y = 0
+        For y = 0 To NumOfLayers - 1
+            For x = 0 To NumOfLegs - 1
+
+                Body.FindPivot(line(x, y), Floor)
+
+            Next
+        Next
+
+        'make the body fall over based on the pivot
+        Body.FallOver(g, line)
+
+        'check if max angle of legs has been reached then rotate legs.
+        For y = 0 To NumOfLayers - 1
+            For x = 0 To NumOfLegs - 1
+
+                line(x, y).AngleLock(Floor)
+                line(x, y).NewPoints()
+
+            Next
+        Next
+
+        'draw the floor
+
+
+        'attach the legs to the body
+        AttachLegs()
+
+        'Find lowest leg past the floor
+        Body.BodyRise = 0
+        For x = 0 To NumOfLegs - 1
+            line(x, 1).FindLowestLeg(line(x, 0), Body.BodyRise)
+        Next
+
+        'raise the body by the distance between the furthest leg and the floor
+        Body.RaiseBody(Floor)
+
+        'attach the legs to the body
+        AttachLegs()
+
+        'drop the body
+        Body.Drop()
+
+        'check if hit floor and if so stop falling
+        For y = 0 To NumOfLayers - 1
+            For x = 0 To NumOfLegs - 1
+                line(x, y).StopFalling(Body, Floor)
+            Next
+        Next
+
+        'attach the legs
+        AttachLegs()
+
+        'find the furthest leg past the floor
+        Body.BodyRise = 0
+        For x = 0 To NumOfLegs - 1
+
+            line(x, 0).Pivot = New Point(0, 0)
+            line(x, 1).Pivot = New Point(0, 0)
+            line(x, 1).FindLowestLeg(line(x, 0), Body.BodyRise)
+
+        Next
+
+        'raise the body by the distance between the furthest leg and the floor
+        Body.RaiseBody(Floor)
+
+        'attach the legs to the body
+        AttachLegs()
+
+        'Find the frictional value based on the different frictions from all legs
+        For y = 0 To NumOfLayers - 1
+            For x = 0 To NumOfLegs - 1
+                Body.FindFriction(line(x, y), Floor)
+            Next
+        Next
+
+        'add the friction to the body that moves it by the distance moved by the leg in sum.
+        Body.AddFriction()
+
+        'attach the legs to the body
+        AttachLegs()
+
+        For y = 0 To NumOfLayers - 1
+            For x = 0 To NumOfLegs - 1
+
+                line(x, y).Draw(g)
+                Joint(x, y).StickToLeg(line(x, y).DrawPoint1)
+                Joint(x, y).Draw(g)
+            Next
+        Next
+        Body.Draw(g)
+
+        IsAlive()
+
+    End Sub
+
+    Sub AttachLegs()
+        'attach top legs to body
+        For x = 0 To NumOfLegs - 1
+            Body.SetPoints(Line(x, 0), x)
+        Next
+
+        'attach bottom legs to top legs
+        For x = 0 To NumOfLegs - 1
+            Line(x, 1).AttachBottomLegs(x, Body.Connections(x), Line(x, 0))
+        Next
+    End Sub
+
+    Sub IsAlive()
+        If Body.CoM.X <= Body.PrevCoM.X Then
+            DeadCounter += 1
+        Else
+            DeadCounter = 0
+            Body.PrevCoM = Body.CoM
+        End If
+    End Sub
+
+End Class
---
+++ A2_project_Clean/A2_project_Clean/Form1.vb
@@ -0,0 +1,106 @@
+﻿Public Class Form1
+    Dim Floor As CFloor
+    Dim NumOfAnimals As Integer = 2
+    Dim Base(NumOfAnimals - 1) As CBody
+    Dim checking As Boolean
+    Public NumOfLegs As Integer = 3
+    Public NumOfLayers As Integer = 2
+    Dim Line(NumOfLegs - 1, NumOfLayers - 1) As CLeg
+    Dim Joint(NumOfLegs - 1, NumOfLayers - 1) As CJoint
+    Dim BodyLines(NumOfAnimals - 1)
+    Dim BodyJoints(NumOfAnimals - 1)
+    Dim Rnd As New Random
+    Dim TotalDistance As Double
+    Dim Animal(NumOfAnimals - 1) As CCreature
+
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'create floor object
+        Floor = New CFloor(400)
+
+        For n = 0 To NumOfAnimals - 1
+            Base(n) = New CBody(100, 150, 300, 150)
+        Next
+
+
+
+
+        For n = 0 To NumOfAnimals - 1
+            Rnd.Next()
+
+            For y = 0 To NumOfLayers - 1
+                For x = 0 To NumOfLegs - 1
+
+                    Line(x, y) = New CLeg(100 + 100 * x, 50 + 100 * y, Rnd)
+                    Joint(x, y) = New CJoint(100 + 100 * x, 50 + 100 * y, 10, 10)
+
+                Next
+            Next
+            BodyLines(n) = Line
+            BodyJoints(n) = Joint
+        Next
+
+
+        For n = 0 To NumOfAnimals - 1
+
+            Animal(n) = New CCreature(Base(n), BodyLines(n), BodyJoints(n), Floor)
+        Next
+
+
+
+    End Sub
+
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'keep display refreshing, loop through display
+        Display.Refresh()
+    End Sub
+
+
+
+
+    Private Sub Display_Paint(sender As Object, e As PaintEventArgs) Handles Display.Paint
+
+
+        Dim g As Graphics
+        g = e.Graphics
+
+        For x = 0 To NumOfAnimals - 1
+            If Animal(x).TimeCounter < 500 And Animal(x).DeadCounter < 125 Then
+
+                Animal(x).NextMove(g)
+            End If
+
+        Next
+
+
+    End Sub
+
+    Function CheckFloor(floor As CFloor, point As Double)
+
+        If floor.ypos - 1 <= point Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+
+    Sub FindFitness(animal() As CCreature)
+        TotalDistance = 0
+        For n = 0 To NumOfAnimals - 1
+            TotalDistance += animal(n).Body.CoM.X
+        Next
+        For n = 0 To NumOfAnimals - 1
+            animal(n).Fitness = animal(n).Body.CoM.X / TotalDistance * 100
+        Next
+
+    End Sub
+
+
+
+End Class
